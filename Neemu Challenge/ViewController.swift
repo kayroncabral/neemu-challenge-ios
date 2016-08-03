c71a3ed90f65f18ba977d90d02b47b2dcfbb4849
Kayron Cabral <kayron.scabral@gmail.com>
2016-08-03 16:15:16 -0400

    Initial Project

---
+++ Neemu Challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Neemu Challenge
+//
+//  Created by Kayron Cabral on 03/08/16.
+//  Copyright © 2016 Neemu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Neemu ChallengeTests/Neemu_ChallengeTests.swift
@@ -0,0 +1,36 @@
+//
+//  Neemu_ChallengeTests.swift
+//  Neemu ChallengeTests
+//
+//  Created by Kayron Cabral on 03/08/16.
+//  Copyright © 2016 Neemu. All rights reserved.
+//
+
+import XCTest
+@testable import Neemu_Challenge
+
+class Neemu_ChallengeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
